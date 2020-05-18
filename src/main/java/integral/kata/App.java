0261133d7c39f5903aec33974ea2c997fefd2bec
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-18 14:37:57 -0400

    App.java: Add coreLogic() method stub & documentation

--- src/main/java/integral/kata/App.java
+++ src/main/java/integral/kata/App.java
@@ -10,7 +10,41 @@ public final class App {
 
 	public static void main(final String[] args) {
 		System.out.println("Started up the application");
-		// TODO
+		coreLogic();
 		System.out.println("Application ran without errors");
 	}
+
+	/****************************************************************************
+	Feature: Publishing
+		Scenario: Alice publishes messages to her personal timeline.
+			Given Alice has published "I love the weather today."
+			When Alice views her timeline
+			Then Alice sees:
+				"I love the weather today."
+
+	Feature: Timeline
+		Scenario: Alice views Bob's timeline.
+			Given Bob has published "Darn! We lost!"
+			And Bob has published "Good game though."
+			When Alice views Bob's timeline
+			Then Alice sees:
+				Good game though. (1 minute ago)
+				Darn! We lost! (2 minute ago)
+
+	Feature: Following
+		Scenario: Charlie can follow Alice and Bob, and he views an aggregated list of all timelines.
+			Given Alice has published "I love the weather today."
+			And Bob has published "Darn! We lost!"
+			And Bob has published "Good game though."
+			And Charlie has published "I'm in New York today! Anyone wants to have a coffee?"
+			When Charlie follows Alice
+			And Charlie follows Bob
+			And Charlie views his wall
+			Then Charlie sees:
+				Charlie - I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)
+				Bob - Good game though. (1 minute ago)
+				Bob - Damn! We lost! (2 minutes ago)
+				Alice - I love the weather today (5 minutes ago)
+	****************************************************************************/
+	public static void coreLogic() {}
 }
