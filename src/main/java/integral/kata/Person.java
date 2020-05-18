a834ebe59adc3bca87b80c79a9bbeca8cd4fa64b
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-18 15:08:25 -0400

    Update Person.java

--- src/main/java/integral/kata/Person.java
+++ src/main/java/integral/kata/Person.java
@@ -6,7 +6,9 @@ public final class Person {
 
 	private final Timeline timeline = new Timeline();
 	// linked list is more performant when this scales to hundreds or more items
-	private final LinkedList<Person> followers = new LinkedList<Person>();
+	private final LinkedList<Person> followers = new LinkedList<>();
+	// arraylist is more performant here (better cache locality, etc)
+	private final ArrayList<Person> following = new ArrayList<>();
 	private final String name;
 
 	public Person(final String name_) {
@@ -19,6 +21,7 @@ public final class Person {
 
 	public void follow(final Person person) {
 		person.followers.add(this);
+		following.add(person);
 	}
 
 	public ArrayList<String> showPosts(final Person person) {
@@ -35,6 +38,17 @@ public final class Person {
 
 	public ArrayList<String> viewTimeline(final Person person) {
 		return person.showPostsDirect(this);
+	}
+
+	public ArrayList<String> viewWall() {
+		// it's us, but we want to see the full message
+		final var posts = showPostsCallback(this, false, false);
+		for (final var person: following) {
+			// if we are following them, it's guaranteed
+			// that they have us in their followers list
+			posts.addAll(viewTimeline(person));
+		}
+		return posts;
 	}
 
 	private ArrayList<String> showPostsCallback(
