142fdb9a33a9391515316a21e511894a2dee76ab
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-05-18 14:07:27 -0400

    Add class: App

---
+++ src/main/java/integral/kata/App.java
@@ -0,0 +1,15 @@
+package integral.kata;
+import java.util.ArrayList;
+
+// this project was tested on a custom OpenJDK 15 build
+// it should run on any version of java 10+
+public final class App {
+
+	public final ArrayList<Person> people = new ArrayList<>();
+
+	public static void main(final String[] args) {
+		System.out.println("Started up the application");
+		// TODO
+		System.out.println("Application ran without errors");
+	}
+}
